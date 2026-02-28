6bc1f08c476d8aeb1e299b6e8f300f0ea634e687
LoFeel ZoNe <lofeelzone@gmail.com>
2026-02-28 13:01:20 +0000

    ## Error Type Runtime Error
    ## Error Message
    Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'replicate.delivery',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
